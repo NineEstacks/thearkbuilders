71aa42e364a864682ce8537f538c5677d0e8f04c
NineEstacks <Nine-Estacks@outlook.com>
2023-07-24 09:57:03 +0100

    commit

--- js/main.js
+++ js/main.js
@@ -147,3 +147,29 @@
     })();
 
 })(jQuery);
+
+/* Main Navigation */
+
+(function($) { 
+    $(function() { 
+      
+      $('nav ul li a:not(:only-child)').click(function(e) {
+        $(this).siblings('.nav-dropdown').toggle();
+        // Close one dropdown when selecting another
+        $('.nav-dropdown').not($(this).siblings()).hide();
+        e.stopPropagation();
+      });
+     
+      $('html').click(function() {
+        $('.nav-dropdown').hide();
+      });
+      
+      $('#nav-toggle').click(function() {
+        $('nav ul').slideToggle();
+      });
+      // Hamburger to X toggle
+      $('#nav-toggle').on('click', function() {
+        this.classList.toggle('active');
+      });
+    }); 
+  })(jQuery); 
